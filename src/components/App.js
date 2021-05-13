66c7ff074aeafebe42281ec48670613d7dca1f78
yasin khadem <yasin.khadem77@gmail.com>
2021-05-13 09:40:34 +0430

    setup react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
+import { Switch, Route } from 'react-router-dom'
 
 export default class App extends Component {
     render() {
         return (
             <div>
+                <Switch>
+                    <Route exact path="/" render={() => <h2>home</h2>}></Route>
+                    <Route path="/about" render={() => <h2>about</h2>}></Route>
+                </Switch>
             </div>
         );
     };
