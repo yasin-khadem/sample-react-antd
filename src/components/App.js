a614d1f39963766bef606a479db05bb94ecd207d
yasin khadem <yasin.khadem77@gmail.com>
2021-05-13 09:30:03 +0430

    setup antd

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                
             </div>
         );
     };
